e0b56d14935f8eacabcee536370da9553f5f0572
Joy <joyglory213@gmail.com>
2023-09-19 22:23:59 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return(`This Saturday, I want to ${activity}!`);
+}
+saturdayFun();
+saturdayFun("bathe my dog");
+
+
+const mondayWork = function(activity = "go to the office"){
+    return(`This Monday, I will ${activity}.`)
+}
+mondayWork();
+mondayWork("work from home");
+
+function wrapAdjective(emphatic){
+    return function(adjective){
+        return `You are ${emphatic}${adjective}${emphatic}!`;
+    }
+}
+wrapAdjective();
+
